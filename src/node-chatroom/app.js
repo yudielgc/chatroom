7a66ed928cc3a3abc566c89e026ae17b5325cc94
yudielgc <yudielgc@gmail.com>
2015-11-25 02:27:43 -0500

    Adding Socket.IO to the chatroom

--- src/node-chatroom/app.js
+++ src/node-chatroom/app.js
@@ -32,6 +32,9 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function () {
+var serve = http.createServer(app);
+var io = require('socket.io')(serve);
+
+serve.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
