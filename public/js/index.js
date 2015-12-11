3d6885b66a6ccdea7c04b34c30302d7d621dc981
yudielgc <yudielgc@gmail.com>
2015-12-10 22:01:41 -0500

    login and identifying users in the chatroom

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,23 @@
 (function () {
     'use strict';
 
+    var userName = '';
+
+    if (sessionStorage.getItem('user') === null) {
+        //$('#username-box').css('display', 'block');
+        toggleLogin();
+    }
+
+    $('#login-btn').click(function () {
+        userName = $('#username-box').val();
+        if (userName !== '') {
+            toggleLogin('close');
+            sessionStorage.setItem('user', userName);
+        }
+
+        return false;
+    });
+
     var socket = io();
     $('#send-message-btn').click(function () {
         sendMessage();
@@ -13,9 +30,14 @@
     });
 
     function sendMessage(){
-        var msg = $('#message-box').val();
+        var msg = userName + ': ' + $('#message-box').val();
         socket.emit('chat', msg);
         $('#messages').append($('<p>').text(msg));
         $('#message-box').val('');
+    }
+
+    function toggleLogin(close){
+        $('#light-box').css('display', close !== 'close' ? 'block' : 'none');
+        $('#overlay').css('display', close !== 'close' ? 'block' : 'none');
     }
 })();
